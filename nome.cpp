0d65f07cd5d89182b24065e6ee5edd5b5380e884
JULIANO MARTINS SCHERER <04471070037@senacrs.edu.br>
2024-04-25 21:43:18 -0300

    a

---
+++ nome.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a = 0;
+    while (a < 10);
+    {
+      cout << "doihsea" << endl;
+      a++;
+    }
+    
+
+
+    return 0;
+}
+
+//para ir de 0 a 10 acrescentando i
+for (int = 0 ; i < 10; i++)
